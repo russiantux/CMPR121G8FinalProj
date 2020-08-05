91456e9a17b6f37c71ab2794d65f07100001ccf5
Trevor <tstike44@hotmail.com>
2020-08-05 13:30:52 -0700

    added more functionality to patient model. will allow multiple entries

--- patient.cpp
+++ patient.cpp
@@ -15,31 +15,62 @@ int main()
 	string provider;
 	int phoneNumber;
 	int age;
+	char userCont;
+	bool end = false;
 
 	fstream saveFile;
-	saveFile.open("database.txt", ios::out);
-	
-	//write user info to file
-	cout << "please enter the following information" << endl;
 
-	cout << "please enter first name" <<endl;
-	cin >> firstName;
+	while (end == false)
+	{
+		saveFile.open("database.txt", ios::out);
 
-	cout << "please enter last name" << endl;
-	cin >> lastName;
-	 
-	cout << "please enter phone number" << endl;
-	cin >> phoneNumber;
+		//write user info to file
+		cout << "\n****** PLEASE ENTER THE FOLLOWING INFORMATION *****\n" << endl;
 
-	cout << "please enter patient age" << endl;
-	cin >> age;
+		cout << "please enter first name\n" << endl;
+		cin >> firstName;
+		saveFile << firstName << setw(5);
 
-	cout << "please enter patients insurance provider" << endl;
-	cin >> provider;
+		cout << "please enter last name\n" << endl;
+		cin >> lastName;
+		saveFile << lastName << setw(5);
 
-	patientInfo.addPatient(firstName, lastName, phoneNumber, age, provider);
-	
-	saveFile << firstName << lastName << ": " << phoneNumber << age << provider << endl;
+		cout << "please enter phone number\n" << endl;
+		cin >> phoneNumber;
+		saveFile << phoneNumber << setw(5);
 
+		cout << "please enter patient age\n" << endl;
+		cin >> age;
+		saveFile << age << setw(5);
+
+		cout << "please enter patients insurance provider\n" << endl;
+		cin >> provider;
+		saveFile << provider << endl;
+
+		patientInfo.addPatient(firstName, lastName, phoneNumber, age, provider);
+
+		//continue?
+		cout << "press Y to add another patient or any other button to exit" << endl << endl;
+		cin >> userCont;
+
+		if (userCont == 'y' | userCont == 'Y')
+		{
+			//continue the loop
+			bool end = false;
+			return  end;
+
+		}
+		else {
+			//end the while loop
+			bool end = true;
+			return end;
+		}
+	}
+
+	saveFile.close();
 	return 0;
 };
+
+
+
+
